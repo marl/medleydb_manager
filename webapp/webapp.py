7c80a3daee6cc648ef5acd334a3141e0ecd6f99a
Hanna Yip <hmyip1@gmail.com>
2016-06-21 17:28:50 -0400

    Created table with name, email, and multitrack name

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -5,19 +5,41 @@ import shutil
 import logging
 import tempfile
 import operator
+import sqlite3
 
 from flask import Flask, jsonify, render_template, request
 
 
 APP = Flask(__name__)
 
+APP.config.update(dict(
+    DATABASE=os.path.join(APP.root_path, 'test.db')
+))
+
+def connect_db():
+    rv = sqlite3.connect(APP.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 @APP.route('/')
 def index():
     # this loads the homepage
     myoptvars = 2
-    x = {'date':[u'2012-06-28', u'2012-06-29', u'2012-06-30'], 'users': [405, 368, 119]}
-    return render_template('mywebsite.html', x=x)
+    rv = connect_db()
+    c = rv.cursor()
+
+    tabledata = {'name': [], 'email': [], 'multitrack_name': []}
+
+    for row in c.execute('SELECT * FROM tickets'):
+        tabledata['name'].append(row[0])
+        tabledata['email'].append(row[1])
+        tabledata['multitrack_name'].append(row[2])
+
+    x = {
+        'date':[u'2012-06-28', u'2012-06-29', u'2012-06-30'],
+        'users': [405, 368, 119]
+    }
+    return render_template('mywebsite.html', x=tabledata)
 
 
 @APP.route('/api/flufluflu')
