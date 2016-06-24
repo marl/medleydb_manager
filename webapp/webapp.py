72b9fccb81a03dfe3812e3e9f62b99776bb8be59
Hanna Yip <hmyip1@gmail.com>
2016-06-24 16:06:53 -0400

    distinguished request to record and general ticket, got view tickets working

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -33,18 +33,24 @@ def fill_table(headers, cursor):
 
     return table
 
+
 def get_header(table_name):
     rv = connect_db()
-    c = rv.cursor()
     cursor = rv.execute('select * from {}'.format(table_name))
     column_headers = list(map(lambda x: x[0], cursor.description))
 
     return column_headers
 
+
 @APP.route('/')
 def index():
     # this loads the homepage
     return render_template('home.html')
+
+
+@APP.route('/requestrecord')
+def request_record():
+    return render_template('requestrecord.html', var1=None, var2=None)
 
 
 @APP.route('/newticket')
@@ -54,15 +60,19 @@ def new_ticket():
 
 @APP.route('/viewtickets')
 def view_tickets():
+    rv = connect_db()
+    cursor = rv.execute('select * from tickets')
     column_headers = get_header('tickets')
 
     tickets = {}
     for name in column_headers:
         tickets[name] = []
+    tickets['url']=[]
 
     for row in cursor:
         for name, item in zip(column_headers, row):
             tickets[name].append(item)
+        tickets['url'].append('/ticket?id={}'.format(row[0]))
 
     return render_template('viewtickets.html', tickets=tickets, column_headers=column_headers)
 
@@ -77,27 +87,28 @@ def band_names():
 
 # This is for internal functions, like adding things to the database
 @APP.route('/ticket')
-def viewticket():
+def ticket():
     """
     view a single ticket
     """
     # code that does things
     ticket_id = request.args.get('id')
     rv = connect_db()
-    c = rv.cursor()
-    current_status = rv.execute('select * from tickets where ticket_id={}'.format(ticket_id))
-    ticket_history = rv.execute('select * from ticket_history where ticket_id={}'.format(ticket_id))
-    multitracks_in_ticket = rv.execute('select * from multitracks where ticket_id={}'.format(ticket_id))
-
-    column_headers = get_header('tickets')
+    current_status_headers = get_header('tickets')
     ticket_history_headers = get_header('ticket_history')
-    multitrack_headers = get_header('multitracks')
-
-    tickets = fill_table(ticket_headers, current_status)
+    multitracks_in_ticket_headers = get_header('multitracks')
+    
+    current_status = rv.execute('select * from tickets where ticket_number="{}"'.format(ticket_id))
+    ticket_history = rv.execute('select * from ticket_history where ticket_number="{}"'.format(ticket_id))
+    multitracks_in_ticket = rv.execute('select * from multitracks where ticket_number="{}"'.format(ticket_id))
+    
+    tickets = fill_table(current_status_headers, current_status)
     ticket_history = fill_table(ticket_history_headers, ticket_history)
-    multitracks_in_ticket = fill_table(multitrack_headers, multitracks_in_ticket)
-
-    return render_template('ticket.html', id=ticket_id)
+    multitracks_in_ticket = fill_table(multitracks_in_ticket_headers, multitracks_in_ticket)
+    print tickets
+    return render_template('ticket.html', ticket_id=ticket_id, current_status_headers=current_status_headers, 
+                           ticket_history_headers=ticket_history_headers, multitracks_in_ticket_headers=multitracks_in_ticket_headers,
+                           current_status=tickets, ticket_history=ticket_history, multitracks_in_ticket=multitracks_in_ticket)
 
 
 if __name__ == '__main__':
