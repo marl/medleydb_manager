82f8ae7e6e876d962092a88a5d55fd45028c98bb
Hanna Yip <hmyip1@gmail.com>
2016-06-28 13:54:23 -0400

    changed variable names of webapp.py, changed instructions and home page

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -22,6 +22,7 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+
 def fill_table(headers, cursor):
     table = {}
     for name in headers:
@@ -65,21 +66,23 @@ def new_ticket():
 def view_tickets():
     rv = connect_db()
     cursor = rv.execute('select * from tickets')
-    column_headers = get_header('tickets')
+    tickets_headers = get_header('tickets')
 
     tickets = {}
-    for name in column_headers:
+    for name in tickets_headers:
         tickets[name] = []
-    tickets['url']=[]
 
+    tickets['url']=[]
     for row in cursor:
-        for name, item in zip(column_headers, row):
+        for name, item in zip(tickets_headers, row):
             tickets[name].append(item)
         tickets['url'].append('/ticket?id={}'.format(row[0]))
 
-    formatted_headers = [format_headers(h) for h in column_headers]
+    formatted_tickets_headers = [format_headers(h) for h in tickets_headers]
 
-    return render_template('viewtickets.html', tickets=tickets, db_column_headers=column_headers, formatted_headers=formatted_headers)
+    return render_template('viewtickets.html', tickets=tickets, 
+        db_tickets_headers=tickets_headers, 
+        formatted_tickets_headers=formatted_tickets_headers)
 
 
 @APP.route('/instructions')
@@ -96,38 +99,39 @@ def ticket():
     """
     view a single ticket
     """
-    # code that does things
     ticket_id = request.args.get('id')
     rv = connect_db()
-    current_status_headers = get_header('tickets')
+    ticket_status_headers = get_header('tickets')
     ticket_history_headers = get_header('ticket_history')
     multitracks_in_ticket_headers = get_header('multitracks')
 
-    formatted_headers1 = [format_headers(h) for h in current_status_headers]
-    formatted_headers2 = [format_headers(h) for h in ticket_history_headers]
-    formatted_headers3 = [format_headers(h) for h in multitracks_in_ticket_headers]
+    formatted_ticket_status_headers = [format_headers(h) for h in ticket_status_headers]
+    formatted_ticket_history_headers = [format_headers(h) for h in ticket_history_headers]
+    formatted_multitracks_in_ticket_headers = [format_headers(h) for h in multitracks_in_ticket_headers]
     
-    current_status = rv.execute('select * from tickets where ticket_number="{}"'.format(ticket_id))
-    ticket_history = rv.execute('select * from ticket_history where ticket_number="{}"'.format(ticket_id))
+    ticket_status_cursor = rv.execute('select * from tickets where ticket_number="{}"'.format(ticket_id))
+    ticket_history_cursor = rv.execute('select * from ticket_history where ticket_number="{}"'.format(ticket_id))
     multitracks_cursor = rv.execute('select * from multitracks where ticket_number="{}"'.format(ticket_id))
     
-
-    tickets = fill_table(current_status_headers, current_status)
-    ticket_history = fill_table(ticket_history_headers, ticket_history)
+    tickets = fill_table(ticket_status_headers, ticket_status_cursor)
+    ticket_history = fill_table(ticket_history_headers, ticket_history_cursor)
     multitracks_in_ticket = fill_table(multitracks_in_ticket_headers, multitracks_cursor)
 
     multitracks_in_ticket['url']=[]
-    print "Asdf"
     multitracks_cursor = rv.execute('select * from multitracks where ticket_number="{}"'.format(ticket_id))
     for row in multitracks_cursor:
         multitracks_in_ticket['url'].append('/multitrack?id={}'.format(row[2]))
 
-    print multitracks_in_ticket
-
-    return render_template('ticket.html', ticket_id=ticket_id, db_current_status_headers=current_status_headers, formatted_headers1=formatted_headers1, 
-                           db_ticket_history_headers=ticket_history_headers, formatted_headers2=formatted_headers2, 
-                           db_multitracks_in_ticket_headers=multitracks_in_ticket_headers, formatted_headers3=formatted_headers3,
-                           current_status=tickets, ticket_history=ticket_history, multitracks_in_ticket=multitracks_in_ticket)
+    return render_template('ticket.html', ticket_id=ticket_id, 
+            db_ticket_status_headers=ticket_status_headers, 
+            formatted_ticket_status_headers=formatted_ticket_status_headers, 
+            db_ticket_history_headers=ticket_history_headers, 
+            formatted_ticket_history_headers=formatted_ticket_history_headers, 
+            db_multitracks_in_ticket_headers=multitracks_in_ticket_headers, 
+            formatted_multitracks_in_ticket_headers=formatted_multitracks_in_ticket_headers,
+            tickets=tickets, 
+            ticket_history=ticket_history, 
+            multitracks_in_ticket=multitracks_in_ticket)
 
 @APP.route('/multitrack')
 def multitrack():
@@ -137,24 +141,30 @@ def multitrack():
     multitrack_id = request.args.get('id')
     rv = connect_db()
     
-    current_status_multitrack_headers = get_header('multitracks')
+    multitrack_status_headers = get_header('multitracks')
     multitrack_history_headers = get_header('multitrack_history')
+    
+    formatted_multitrack_status_headers = [format_headers(h) for h in multitrack_status_headers]
+    formatted_multitrack_history_headers = [format_headers(h) for h in multitrack_history_headers]
 
-    current_status = rv.execute('select * from multitracks where multitrack_id="{}"'.format(multitrack_id))
+    multitrack_status_cursor = rv.execute('select * from multitracks where multitrack_id="{}"'.format(multitrack_id))
     multitrack_history_cursor = rv.execute('select * from multitrack_history where multitrack_id="{}"'.format(multitrack_id))
 
-    formatted_headers3 = [format_headers(h) for h in current_status_multitrack_headers]
-    formatted_headers4 = [format_headers(h) for h in multitrack_history_headers]
-
-    multitrack = fill_table(current_status_multitrack_headers, current_status)
+    multitrack = fill_table(multitrack_status_headers, multitrack_status_cursor)
     multitrack_history = fill_table(multitrack_history_headers, multitrack_history_cursor)
 
     return render_template('multitrack.html', multitrack_id=multitrack_id,
-        current_status_multitrack_headers=current_status_multitrack_headers, 
-        multitrack_history_headers=multitrack_history_headers,
-        multitrack=multitrack, multitrack_history=multitrack_history, formatted_headers3=formatted_headers3,
-        formatted_headers4=formatted_headers4)
+        db_multitrack_status_headers=multitrack_status_headers, 
+        formatted_multitrack_status_headers=formatted_multitrack_status_headers,
+        db_multitrack_history_headers=multitrack_history_headers,
+        formatted_multitrack_history_headers=formatted_multitrack_history_headers,
+        multitrack_history=multitrack_history, 
+        multitrack_status=multitrack)
 
 
 if __name__ == '__main__':
     APP.run(port=5080, host='0.0.0.0', debug='--debug' in sys.argv)
+
+
+
+
